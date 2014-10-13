46dbc20bee6018438358a3916efb615660349e4d
djbrooks111 <djbrooks111@gmail.com>
2014-10-13 13:37:35 -0400

    Removed UIKit from .pch as new iOS 8/Xcode 6 does not create/use .pch files. Imported UIKit into the CustomTimePicker.h file

--- CustomTimePicker/UI/CustomTimePicker/CustomTimePicker.h
+++ CustomTimePicker/UI/CustomTimePicker/CustomTimePicker.h
@@ -7,6 +7,7 @@
 //
 
 #import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
 @protocol CustomTimePickerDlegate <NSObject>
 -(void)dismissClockViewWithHours:(NSString *)hours andMinutes:(NSString *)minutes andTimeMode:(NSString *)timeMode;
 @end
